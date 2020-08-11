90cefe06fb26e1fb909b45969ea1cf6cf2db5e8a
TsapkoZh <60929704+TsapkoZh@users.noreply.github.com>
2020-08-11 11:25:11 +0300

    Updates

---
+++ precache-manifest.254b61e7f4df3dfea633cd7d4c7b3bac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6468c9836013ff4ac9277e3373561d16",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "5c8e5ef4dac3c5df272a",
+    "url": "/my-app/static/css/main.9d183ead.chunk.css"
+  },
+  {
+    "revision": "32d6d0e201bbd56f7ab5",
+    "url": "/my-app/static/js/2.ab507f26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.ab507f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8e5ef4dac3c5df272a",
+    "url": "/my-app/static/js/main.1521d6c8.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  }
+]);
