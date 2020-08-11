d7d3d23e32fbd98d78f8fc550e8f13457eb26ba4
TsapkoZh <60929704+TsapkoZh@users.noreply.github.com>
2020-08-11 11:26:10 +0300

    Updates

---
+++ precache-manifest.580df2db31577f1b741e337d1f9ccb23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f99a6499d7e506e82b324ef502e99bf",
+    "url": "/fillingForm/index.html"
+  },
+  {
+    "revision": "5c8e5ef4dac3c5df272a",
+    "url": "/fillingForm/static/css/main.9d183ead.chunk.css"
+  },
+  {
+    "revision": "32d6d0e201bbd56f7ab5",
+    "url": "/fillingForm/static/js/2.ab507f26.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fillingForm/static/js/2.ab507f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c8e5ef4dac3c5df272a",
+    "url": "/fillingForm/static/js/main.1521d6c8.chunk.js"
+  },
+  {
+    "revision": "989f5198db13c8f9c246",
+    "url": "/fillingForm/static/js/runtime-main.3bd53c02.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.254b61e7f4df3dfea633cd7d4c7b3bac.js"
+  "/fillingForm/precache-manifest.580df2db31577f1b741e337d1f9ccb23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fillingForm/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
